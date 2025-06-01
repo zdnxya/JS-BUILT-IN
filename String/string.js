a75e1f13f4632a8e77f4ad1ef46b462deb598914
Zidan <zidanhadiprakoso@gmail.com>
2025-06-01 17:28:21 +0700

    added folder string

---
+++ String/string.js
@@ -0,0 +1,11 @@
+// string formater
+
+const data = "Learning Until Died";
+
+console.log(data.toLocaleLowerCase());
+console.log(data.toLocaleLowerCase());
+console.log(data.split(" "));
+
+const input = "                              THIS INPUT   ";
+
+console.log(input.trim()); // sama kaya di py strip()

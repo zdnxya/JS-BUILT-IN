4f950ac5de56e68ca726d04d7da22476f3985e7c
Zidan <zidanhadiprakoso@gmail.com>
2025-06-02 06:29:20 +0700

    added folder math

---
+++ Math/math.js
@@ -0,0 +1,14 @@
+// math min & max
+
+console.info(Math.min(1,2,3,4,5,6,7,9,10))
+console.info(Math.max(1,2,3,4,5,6,7,9,10))
+
+// round, floor
+
+console.info(Math.round(10.5) + " = 10.5 Round")
+console.info(Math.floor(10.5) + " = 10.5 Floor")
+
+// Math Random
+
+let randomNum = Math.floor(Math.random() * 100) + 1;
+console.info(randomNum)

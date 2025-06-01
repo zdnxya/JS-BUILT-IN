d93d314a541ef3b94f99b879d94cf50d68a00161
Zidan <zidanhadiprakoso@gmail.com>
2025-06-02 06:02:11 +0700

    added folder date

--- Date/date.js
+++ Date/date.js
@@ -11,3 +11,11 @@ console.info(customDate)
 // epoch time
 
 console.info(Date.now())
+
+// parsing date (YYYY(YEAR)-MM(MONTH)-DD-T-HH(DATE-PEMISAH-HOURS)-mm(MINUTES)-ss(SECOND)-sss(mili seconds)-Z)
+
+const parsingDate = new Date("2006-06-27T10:20:20.123+07:00");
+console.log(parsingDate);
+
+const formatParsing = new Date(parsingDate);
+console.info(formatParsing)

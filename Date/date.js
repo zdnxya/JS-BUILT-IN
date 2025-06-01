eb90b9d56d5840d45b8b74ee1c30eedd94ad13c4
Zidan <zidanhadiprakoso@gmail.com>
2025-06-02 05:39:54 +0700

    added folder date

---
+++ Date/date.js
@@ -0,0 +1,13 @@
+// (year, month, day)
+
+// default 
+
+const today = new Date();
+console.info(today)
+
+const customDate = new Date(2020, 6, 27);
+console.info(customDate)
+
+// epoch time
+
+console.info(Date.now())
